a821bf8475e6ff3b86e329a12253c73134734a88
xofred <fwin3000@gmail.com>
2020-08-10 16:43:08 +0800

    fix original #289
    https://github.com/amro/gibbon/issues/289

--- spec/gibbon/api_request_spec.rb
+++ spec/gibbon/api_request_spec.rb
@@ -10,13 +10,13 @@ describe Gibbon::APIRequest do
   end
 
   it "surfaces client request exceptions as a Gibbon::MailChimpError" do
-    exception = Faraday::Error::ClientError.new("the server responded with status 503")
+    exception = Faraday::ClientError.new("the server responded with status 503")
     stub_request(:get, "#{@api_root}/lists").to_raise(exception)
     expect { @gibbon.lists.retrieve }.to raise_error(Gibbon::MailChimpError)
   end
 
   it "surfaces an unparseable client request exception as a Gibbon::MailChimpError" do
-    exception = Faraday::Error::ClientError.new(
+    exception = Faraday::ClientError.new(
       "the server responded with status 503")
     stub_request(:get, "#{@api_root}/lists").to_raise(exception)
     expect { @gibbon.lists.retrieve }.to raise_error(Gibbon::MailChimpError)
@@ -24,7 +24,7 @@ describe Gibbon::APIRequest do
 
   it "surfaces an unparseable response body as a Gibbon::MailChimpError" do
     response_values = {:status => 503, :headers => {}, :body => '[foo]'}
-    exception = Faraday::Error::ClientError.new("the server responded with status 503", response_values)
+    exception = Faraday::ClientError.new("the server responded with status 503", response_values)
 
     stub_request(:get, "#{@api_root}/lists").to_raise(exception)
     expect { @gibbon.lists.retrieve }.to raise_error(Gibbon::MailChimpError)
@@ -33,7 +33,7 @@ describe Gibbon::APIRequest do
   context "handle_error" do
     it "includes status and raw body even when json can't be parsed" do
       response_values = {:status => 503, :headers => {}, :body => 'A non JSON response'}
-      exception = Faraday::Error::ClientError.new("the server responded with status 503", response_values)
+      exception = Faraday::ClientError.new("the server responded with status 503", response_values)
       api_request = Gibbon::APIRequest.new(builder: Gibbon::Request)
       begin
         api_request.send :handle_error, exception
@@ -46,7 +46,7 @@ describe Gibbon::APIRequest do
     context "when symbolize_keys is true" do
       it "sets title and detail on the error params" do
         response_values = {:status => 422, :headers => {}, :body => '{"title": "foo", "detail": "bar"}'}
-        exception = Faraday::Error::ClientError.new("the server responded with status 422", response_values)
+        exception = Faraday::ClientError.new("the server responded with status 422", response_values)
         api_request = Gibbon::APIRequest.new(builder: Gibbon::Request.new(symbolize_keys: true))
         begin
           api_request.send :handle_error, exception
